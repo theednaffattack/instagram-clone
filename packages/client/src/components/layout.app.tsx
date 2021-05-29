d94886fcc1db241b6b71333df826b9e21c8d16a8
Eddie Naff <eddie@eddienaff.dev>
2021-05-28 19:14:47 -0700

    Add state to app layout

--- packages/client/src/components/layout.app.tsx
+++ packages/client/src/components/layout.app.tsx
@@ -1,22 +1,38 @@
 import * as React from "react";
+import { useState } from "react";
 import { ThemeType } from "../pages/_app";
 import { globalStyles } from "../styles/global-styles";
 
 export type LayoutProps = {
   children: React.ReactChildren[] | React.ReactChild;
-  className: string;
-  theme: ThemeType;
-  toggleTheme: () => void;
+  className?: string;
 };
 
 export default function AppLayout({
   children,
-  theme,
-  toggleTheme,
+  className,
 }: LayoutProps): JSX.Element {
+  const [themeState, setThemeState] = useState<ThemeType>("light");
+
+  function toggleTheme(evt: React.MouseEvent) {
+    evt.preventDefault();
+    setThemeState((prevState) => {
+      if (prevState === "light") {
+        return "dark";
+      }
+      if (prevState === "dark") {
+        return "light";
+      }
+    });
+  }
   return (
-    <div id="main-grid" className={globalStyles}>
-      <button onClick={toggleTheme}>{theme}</button>
+    <div
+      id="main-grid"
+      className={className ? className + " " + globalStyles : globalStyles}
+    >
+      <button onClick={toggleTheme} type="button">
+        {themeState}
+      </button>
       {children}
     </div>
   );
