77007e555c4631d60034e1c93eb183a719831ed3
Eddie Naff <eddie@eddienaff.dev>
2021-06-18 12:28:49 -0700

    Extract apollo server configs to functions

---
+++ packages/server/src/config.apollo-context.ts
@@ -0,0 +1,93 @@
+import { ExpressContext } from "apollo-server-express";
+import { MyContext } from "./types";
+
+export function configApolloContext({ req, res, connection }: ExpressContext) {
+  if (connection) {
+    return getContextFromSubscription(connection);
+    // return {
+    //   ...getContextFromSubscription(connection),
+    //   usersLoader: createUsersLoader()
+    // };
+  }
+
+  return getContextFromHttpRequest(req, res);
+
+  // return {
+  //   ...getContextFromHttpRequest(req, res),
+  //   usersLoader: createUsersLoader()
+  // };
+
+  // return { req, res, connection }
+}
+
+const getContextFromHttpRequest = (req: MyContext["req"], res: MyContext["res"]) => {
+  // old cookie implementation
+  if (req && req.session) {
+    const { teamId, userId } = req.session;
+    console.log("I CAN SEE REQ.SESSION");
+
+    return { userId, req, res, teamId };
+  }
+
+  // JWT implementation
+  // const authorization = req.headers["authorization"];
+  // if (authorization) {
+  //   try {
+  //     const token = authorization.split(" ")[1];
+  //     const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+  //     return {
+  //       req,
+  //       res,
+  //       payload: { token: payload },
+  //       token: req.headers.authorization || "",
+  //     };
+  //   } catch (err) {
+  //     console.log("IS AUTH ERROR", err);
+  //     if (err.name === "TokenExpiredError") {
+  //       throw createAuthorizationError({
+  //         message: "Your session has expired, please log in.",
+  //       });
+  //     }
+  //     throw createAuthorizationError({ message: "Not Authorized." });
+
+  //     // throw new Error("Not authenticated");
+  //     // return {
+  //     //   req,
+  //     //   res,
+  //     //   payload: {
+  //     //     token: undefined,
+  //     //     errors: [err],
+  //     //   },
+  //     //   token: req.headers.authorization || "",
+  //     // };
+  //   }
+  // } else {
+  //   return { req, res };
+  // }
+};
+
+const getContextFromSubscription = (connection: any) => {
+  // old cookie implementation
+  const { userId } = connection.context.req.session;
+
+  return { req: connection.context.req, res: connection.context.res, userId };
+  // JSON Web token implementation
+  // const authorization = connection.context.authorization;
+  // if (authorization) {
+  //   try {
+  //     const token = authorization.split(" ")[1];
+  //     const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+  //     return {
+  //       payload: { token: payload },
+  //       req: connection.context.req,
+  //       res: connection.context.res,
+  //       teamId: connection.context.teamId,
+  //       token: authorization,
+  //     };
+  //   } catch (error) {
+  //     throw Error("Error authenticating subscription.");
+  //   }
+  // } else {
+  //   return { req: connection.context.req, res: connection.context.res };
+  // }
+};
---
+++ packages/server/src/config.apollo-subscriptions.ts
@@ -0,0 +1,74 @@
+import { SubscriptionServerOptions } from "apollo-server-core";
+import session from "express-session";
+import type { RequestHandler } from "express";
+import type { ParsedQs } from "qs";
+import type { ParamsDictionary } from "express-serve-static-core";
+import connectRedis from "connect-redis";
+
+import { redis } from "./redis";
+import { redisSessionPrefix } from "./constants";
+import { ServerConfigProps } from "./config.build-config";
+
+const RedisStore = connectRedis(session);
+
+type SessionMiddle = RequestHandler<ParamsDictionary, any, any, ParsedQs, Record<string, any>>;
+
+export function configSessionMiddleware(config: ServerConfigProps): SessionMiddle {
+  let sessionMiddleware: SessionMiddle;
+
+  // old cookie implentation
+  if (config.env === "production") {
+    sessionMiddleware = session({
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
+        domain: config.domain,
+        path: "/",
+      },
+      name: config.cookieName,
+      resave: false,
+      saveUninitialized: false,
+      secret: config.secret,
+      store: new RedisStore({
+        client: redis as any,
+        prefix: redisSessionPrefix,
+      }),
+    });
+  } else {
+    sessionMiddleware = session({
+      name: config.cookieName,
+      secret: config.secret,
+      store: new RedisStore({
+        client: redis as any,
+        prefix: redisSessionPrefix,
+      }),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        // secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
+        domain: config.domain,
+      },
+    });
+  }
+  return sessionMiddleware;
+}
+
+export function configGraphQLSubscriptions(sessionMiddleware: any): Partial<SubscriptionServerOptions> {
+  return {
+    path: "/subscriptions",
+    onConnect: (_, ws: any) => {
+      console.log("Client connected");
+      return new Promise((res) =>
+        sessionMiddleware(ws.upgradeReq, {} as any, () => {
+          res({ req: ws.upgradeReq });
+        })
+      );
+    },
+    onDisconnect: (_webSocket, _context) => {
+      console.log("Client disconnected");
+    },
+  };
+}
