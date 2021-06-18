512fcc90fb2db67ad0e77be63ef71efd1ea7fc8d
Eddie Naff <eddie@eddienaff.dev>
2021-06-18 16:23:11 -0700

    Import entities from slack clone

---
+++ packages/server/src/entity.file.ts
@@ -0,0 +1,103 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import {
+  Field,
+  ID,
+  ObjectType,
+  registerEnumType,
+  InputType,
+  // InputType
+} from "type-graphql";
+
+import { User } from "./entity.user";
+import { Message } from "./entity.message";
+
+/** Basic Known File Types */
+export enum FileTypeEnum {
+  CSS = "CSS",
+  CSV = "CSV",
+  IMAGE = "IMAGE",
+  PDF = "PDF",
+  SVG = "SVG",
+  MD = "MD",
+  DOC = "DOC",
+  OTHER = "OTHER",
+}
+
+registerEnumType(FileTypeEnum, {
+  name: "FileTypeEnum", // this one is mandatory
+  description: "css | csv | image-all | pdf | svg | docx | other", // this one is optional
+});
+
+@ObjectType()
+@Entity()
+export class File extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Field()
+  @Column()
+  uri: string;
+
+  @Field(() => FileTypeEnum, { nullable: false })
+  @Column({
+    type: "enum",
+    enum: FileTypeEnum,
+    default: FileTypeEnum.OTHER,
+  })
+  file_type: FileTypeEnum;
+
+  @Field(() => Message, { nullable: true })
+  @ManyToOne(() => Message, (message) => message.files, {
+    nullable: true,
+    onDelete: "CASCADE",
+  })
+  message?: Message;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.files, { onDelete: "CASCADE" })
+  upload_user: User;
+}
+
+@InputType()
+export class FileInputHelper {
+  @Field()
+  @Column()
+  uri: string;
+
+  // @Field()
+  // @Column()
+  // type: string;
+
+  // @Field()
+  // @Column()
+  // lastModified: string;
+
+  // @Field()
+  // @Column()
+  // lastModifiedDate: Date;
+
+  // @Field()
+  // @Column()
+  // name: string;
+
+  // @Field()
+  // @Column()
+  // path: string;
+
+  // @Field()
+  // @Column()
+  // webkitRelativePath: string;
+
+  // @Field()
+  // @Column()
+  // size: number;
+
+  @Field(() => FileTypeEnum, { nullable: false })
+  @Column({
+    type: "enum",
+    enum: FileTypeEnum,
+    default: FileTypeEnum.OTHER,
+  })
+  file_type: FileTypeEnum;
+}
--- packages/server/src/entity.user.ts
+++ packages/server/src/entity.user.ts
@@ -1,24 +1,119 @@
-import { ID, Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from "typeorm";
+import { Field, ID, ObjectType, Root } from "type-graphql";
 
+import { Thread } from "./entity.thread";
+import { Message } from "./entity.message";
+import { File } from "./entity.file";
+
+/**
+ * User Entity (model)
+ * @param {string} User.id - The ID of a User
+ * @param {string} User.firstName - The given name of a User
+ * @param {string} User.lastName - The family name (surname) of a User
+ */
 @ObjectType()
-export class User {
+@Entity()
+export class User extends BaseEntity {
+  /**id field */
   @Field(() => ID, { nullable: true })
-  id!: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-  @Field(() => String, { nullable: true })
-  firstName!: string;
+  @Field({ nullable: true })
+  @Column()
+  firstName: string;
 
-  @Field(() => String, { nullable: true })
-  lastName!: string;
+  @Field({ nullable: true })
+  @Column()
+  lastName: string;
 
-  @Field(() => String, { nullable: true })
-  username!: string;
+  @Field({ nullable: true })
+  @Column("text", { unique: true })
+  username: string;
 
-  @Field(() => String, { nullable: true })
-  email!: string;
+  @Field({ nullable: true })
+  @Column("text", { unique: true })
+  email: string;
 
-  @Field(() => String, { nullable: true })
-  confirmed!: boolean;
+  @Field(() => [File], { nullable: "itemsAndList" })
+  @OneToMany(() => File, (file) => file.upload_user, {
+    nullable: true,
+    onDelete: "CASCADE",
+  })
+  files: File[];
 
-  password!: string;
+  @Field(() => [Message], { nullable: "itemsAndList" })
+  mappedMessages: Message[];
+
+  @Field(() => [User], { nullable: "itemsAndList" })
+  @ManyToMany(() => User, (user) => user.following, {
+    nullable: true,
+    onDelete: "CASCADE",
+  })
+  @JoinTable()
+  followers: User[];
+
+  @Field(() => [User], { nullable: "itemsAndList" })
+  @ManyToMany(() => User, (user) => user.followers, {
+    nullable: true,
+    onDelete: "CASCADE",
+  })
+  following: User[];
+
+  // @Field(() => Channel, { nullable: true })
+  // @ManyToOne(() => Channel, (channel) => channel.created_by, {
+  //   nullable: true,
+  //   onDelete: "CASCADE",
+  // })
+  // channels_created?: Channel;
+
+  // @Field(() => [Channel], { nullable: "itemsAndList" })
+  // @ManyToMany(() => Channel, (channel) => channel.invitees, { nullable: true })
+  // @JoinTable()
+  // channel_memberships: Channel[];
+
+  @Field({ nullable: true })
+  @Column("text", { unique: true, nullable: true })
+  profileImageUri: string;
+
+  @Field({ nullable: true })
+  name(@Root() parent: User): string {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+
+  @Column()
+  password: string;
+
+  @Column("bool", { default: false })
+  confirmed: boolean;
+
+  @Field(() => [Message], { nullable: true })
+  @OneToMany(() => Message, (message) => message.user, {
+    onDelete: "CASCADE",
+  })
+  messages?: Message[];
+
+  @Field(() => [Message], { nullable: true })
+  @OneToMany(() => Message, (message) => message.sentBy, {
+    onDelete: "CASCADE",
+  })
+  sent_messages: Message[];
+
+  @Field(() => [Thread], { nullable: "itemsAndList" })
+  @OneToMany(() => Thread, (thread) => thread.user)
+  threads?: Thread[];
+
+  @Field(() => [Thread], { nullable: "itemsAndList" })
+  @ManyToMany(() => Thread, (thread) => thread.invitees, { nullable: true })
+  @JoinTable()
+  thread_invitations: Thread[];
 }
