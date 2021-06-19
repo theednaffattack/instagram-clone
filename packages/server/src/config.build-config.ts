9be309fa87a08dbdd240895c783560cedba73c8c
Eddie Naff <eddie@eddienaff.dev>
2021-06-19 12:55:46 -0700

    Redo convict settings

--- packages/server/src/config.build-config.ts
+++ packages/server/src/config.build-config.ts
@@ -1,175 +1,146 @@
 import convict from "convict";
 import { ipaddress, url } from "convict-format-with-validator";
-import { readFileSync } from "fs";
-import { v4 } from "internal-ip";
 
 convict.addFormat(ipaddress);
 convict.addFormat(url);
 
+export interface ServerConfigProps {
+  allowedOrigins: string;
+  client_uri: string;
+  cookieName: string;
+  domain: string;
+  host: string;
+  db: {
+    connectionString: string;
+    host: string;
+    name: string;
+    username: string;
+    password: string;
+    port: string;
+  };
+  env: string;
+  apiEndpoint: string;
+  ip: string;
+  port: number;
+  postmarkToken: string;
+  secret: string;
+}
+
 // export async function buildConfig(): Promise<ServerConfigProps> {
 // Define a schema
-const configBuilder = convict({
-  client_uri: {
-    default: "https://ic.eddienaff.dev",
-    doc: "The URI of our client application.",
-    format: "url",
-  },
-  cookieName: {
-    default: "icg",
-    doc: "Cookie name used for our sessions.",
-    env: "COOKIE_NAME",
-    format: String,
-  },
-  domain: {
-    default: "ic.eddienaff.dev",
-    doc: "The hostname to use for linking and documentation references.",
-    env: "DOMAINS",
-    format: String,
-  },
-  host: {
-    default: "0.0.0.0",
-    doc: "Hostname we bind the server to",
-    env: "DOMAINS",
-    format: String,
-  },
-  db: {
-    connectionString: {
-      default: "postgres://spc_system:R4TDP6g37Lzkh3Fc5U8zGvfZx7@localhost:5432/spotify_clone",
-      doc: "The database connection string",
-      env: "DATABASE_URL",
+export const configBuildAndValidate = async function () {
+  const configBuilder = convict({
+    allowedOrigins: {
+      doc: "Allowed list of CORS origins",
+      default: "ic.eddienaff.dev",
+      env: "ALLOWED_ORIGINS",
+      format: String,
+    },
+    client_uri: {
+      default: "https://ic.eddienaff.dev",
+      doc: "The URI of our client application.",
+      format: "url",
+    },
+    cookieName: {
+      default: "icg",
+      doc: "Cookie name used for our sessions.",
+      env: "COOKIE_NAME",
+      format: String,
+    },
+    domain: {
+      default: "ic.eddienaff.dev",
+      doc: "The hostname to use for linking and documentation references.",
+      env: "DOMAINS",
       format: String,
     },
     host: {
-      default: "ic.eddienaff.dev",
-      doc: "Database host name/IP",
-      format: "*",
-    },
-    name: {
-      default: "spotify_clone",
-      doc: "Database name",
-      env: "POSTGRES_DBNAME",
+      default: "0.0.0.0",
+      doc: "Hostname we bind the server to",
+      env: "DOMAINS",
       format: String,
     },
-    username: {
-      default: "ic_user",
-      doc: "Postgres username for database login.",
-      env: "POSTGRES_USER",
+    db: {
+      connectionString: {
+        default: "postgres://spc_system:R4TDP6g37Lzkh3Fc5U8zGvfZx7@localhost:5432/spotify_clone",
+        doc: "The database connection string",
+        env: "DATABASE_URL",
+        format: String,
+      },
+      host: {
+        default: "ic.eddienaff.dev",
+        doc: "Database host name/IP",
+        format: "*",
+      },
+      name: {
+        default: "spotify_clone",
+        doc: "Database name",
+        env: "POSTGRES_DBNAME",
+        format: String,
+      },
+      username: {
+        default: "ic_user",
+        doc: "Postgres username for database login.",
+        env: "POSTGRES_USER",
+        format: String,
+      },
+      password: {
+        default: "*******",
+        doc: "Postgres password for database login.",
+        env: "POSTGRES_PASSWORD",
+        format: String,
+      },
+      port: {
+        default: "5432",
+        doc: "Postgres connection port",
+        env: "POSTGRES_PORT",
+        format: Number,
+      },
+    },
+    env: {
+      default: "development",
+      doc: "The application environment.",
+      env: "NODE_ENV",
+      format: ["production", "local_production", "development", "test"],
+    },
+    apiEndpoint: {
+      default: "/api",
+      doc: "The slash address of our GraphQl uri",
+      env: "API_ENDPOINT",
       format: String,
     },
-    password: {
-      default: "*******",
-      doc: "Postgres password for database login.",
-      env: "POSTGRES_PASSWORD",
-      format: String,
+    ip: {
+      default: "127.0.0.1",
+      doc: "The IP address to bind.",
+      env: "IP_ADDRESS",
+      format: "ipaddress",
     },
     port: {
-      default: "5432",
-      doc: "Postgres connection port",
-      env: "POSTGRES_PORT",
-      format: Number,
+      arg: "port",
+      default: 8080,
+      doc: "The port to bind.",
+      env: "PORT",
+      format: "port",
     },
-  },
-  env: {
-    default: "development",
-    doc: "The application environment.",
-    env: "NODE_ENV",
-    format: ["production", "development", "test"],
-  },
-  apiEndpoint: {
-    default: "/api",
-    doc: "The slash address of our GraphQl uri",
-    env: "API_ENDPOINT",
-    format: String,
-  },
-  ip: {
-    default: "127.0.0.1",
-    doc: "The IP address to bind.",
-    env: "IP_ADDRESS",
-    format: "ipaddress",
-  },
-  port: {
-    arg: "port",
-    default: 8080,
-    doc: "The port to bind.",
-    env: "PORT",
-    format: "port",
-  },
-  postmarkToken: {
-    default: "",
-    doc: "Secret token used to access the Postmark mailing API.",
-    env: "POSTMARK_API_TOKEN",
-    format: "*",
-    sensitive: true,
-  },
-  secret: {
-    default: "",
-    doc: "Secret used for session cookies and CSRF tokens",
-    env: "SESSION_SECRET",
-    format: "*",
-    sensitive: true,
-  },
-});
+    postmarkToken: {
+      default: "",
+      doc: "Secret token used to access the Postmark mailing API.",
+      env: "POSTMARK_API_TOKEN",
+      format: "*",
+      sensitive: true,
+    },
+    secret: {
+      default: "",
+      doc: "Secret used for session cookies and CSRF tokens",
+      env: "SESSION_SECRET",
+      format: "*",
+      sensitive: true,
+    },
+  });
 
-// Load environment dependent configuration
-const env = configBuilder.get("env");
+  // Perform validation
+  return configBuilder.validate({ allowed: "strict" });
+};
 
-// configBuilder.loadFile(`${__dirname}/secret.${env}-variables.json`);
+// const config = configBuilder.getProperties();
 
-let host: string | undefined;
-if (env === "development") {
-  try {
-    host = v4.sync();
-  } catch (error) {
-    console.error("Error determining IP address.", error);
-    host = undefined;
-  }
-
-  // // override default settings (load env file) to use development
-  // // settings instead.
-  // configBuilder.loadFile(`${__dirname}/secret.${env}-variables.json`);
-
-  let configOverride;
-  try {
-    configOverride = readFileSync(`${__dirname}/secret.${env}-variables.json`, "utf-8");
-  } catch (error) {
-    console.error(`ERROR LOADING CONFIGURATION OVERRIDE FILE: ${__dirname}/secret.${env}-variables.json`, error);
-    throw Error(error);
-  }
-
-  // Override values set via .env file by manually loading, parsing,
-  // and looping over it.
-  for (const [key, value] of Object.entries(JSON.parse(configOverride))) {
-    // If the value is not an object we don't need
-    // traverse it.
-    if (value && typeof value !== "object") {
-      configBuilder.set(key, value);
-    }
-    // If the value IS AN OBJECT we break up it's
-    // values and loop over it, setting our config values
-    // in the loop
-    if (value && typeof value === "object") {
-      const what = value;
-      for (const [nestedKey, nestedValue] of Object.entries(value)) {
-        configBuilder.set(`${key}.${nestedKey}`, nestedValue);
-      }
-    }
-  }
-
-  // Set a few values that don't make as much
-  // sense (naming-wise) for local dev.
-  if (typeof host === "string") {
-    configBuilder.set("domain", host);
-    configBuilder.set("host", host);
-    configBuilder.set("db.host", host);
-    configBuilder.set("ip", host);
-  }
-}
-
-// Perform validation
-configBuilder.validate({ allowed: "strict" });
-
-const config = configBuilder.getProperties();
-
-export { config };
-
-export type ServerConfigProps = typeof config;
+// export { config };
