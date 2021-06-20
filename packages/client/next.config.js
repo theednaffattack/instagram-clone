8d649053ab3ae2809eba4f251b623ad94bf3370c
Eddie Naff <eddie@eddienaff.dev>
2021-06-20 08:15:41 -0700

    Upgrade deps needed for linaria

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,4 +1,9 @@
-const withCSS = require("@zeit/next-css");
+// const withCSS = require("@zeit/next-css");
+
+// const withLinaria = require("next-linaria");
+
+// module.exports = withLinaria({});
+
 const LINARIA_EXTENSION = ".linaria.module.css";
 
 function traverse(rules) {
@@ -10,6 +15,10 @@ function traverse(rules) {
         typeof rule.options.modules.getLocalIdent === "function"
       ) {
         let nextGetLocalIdent = rule.options.modules.getLocalIdent;
+        rule.options.modules.mode = "local";
+        rule.options.modules.auto = true;
+        rule.options.modules.exportGlobals = true;
+        rule.options.modules.exportOnlyLocals = false;
         rule.options.modules.getLocalIdent = (
           context,
           _,
@@ -32,101 +41,44 @@ function traverse(rules) {
   }
 }
 
-module.exports = withCSS({
-  webpack(config, options) {
-    traverse(config.module.rules);
-    config.module.rules.push({
-      test: /(?!_app)\.(tsx|ts|js|mjs|jsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: require.resolve("@linaria/webpack-loader"),
-          options: {
-            sourceMap: process.env.NODE_ENV !== "production",
-            ...(config.linaria || {}),
-            extension: LINARIA_EXTENSION,
+module.exports = (nextConfig = {}) => {
+  return {
+    ...nextConfig,
+    webpack(config, options) {
+      traverse(config.module.rules);
+      config.module.rules.push({
+        test: /(?!_app)\.(tsx|ts|js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("@linaria/webpack-loader"),
+            options: {
+              sourceMap: process.env.NODE_ENV !== "production",
+              ...(nextConfig.linaria || {}),
+              extension: LINARIA_EXTENSION,
+            },
           },
-        },
-      ],
-    });
-    config.module.rules.push({
-      test: /_app\.(tsx|ts|js|mjs|jsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: require.resolve("@linaria/webpack-loader"),
-          options: {
-            sourceMap: process.env.NODE_ENV !== "production",
-            ...(config.linaria || {}),
-            extension: ".css",
+        ],
+      });
+      config.module.rules.push({
+        test: /_app\.(tsx|ts|js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("@linaria/webpack-loader"),
+            options: {
+              sourceMap: process.env.NODE_ENV !== "production",
+              ...(nextConfig.linaria || {}),
+              extension: ".css",
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    if (typeof config.webpack === "function") {
-      return config.webpack(config, options);
-    }
-    return config;
-  },
-});
-
-// // old linaria config from linaria docs
-// module.exports = withCSS({
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.(js|jsx|ts|tsx)$/, // /\.tsx$/,
-//       use: [
-//         {
-//           loader: "linaria/loader",
-//           options: {
-//             sourceMap: process.env.NODE_ENV !== "production",
-//           },
-//         },
-//       ],
-//     });
-
-//     return config;
-//   },
-// });
-
-// const withLinaria = require("next-linaria");
-
-// module.exports = withLinaria({
-//   webpack(config, options) {
-//     traverse(config.module.rules);
-//     config.module.rules.push({
-//       test: /(?!_app)\.(tsx|ts|js|mjs|jsx)$/,
-//       exclude: /node_modules/,
-//       use: [
-//         {
-//           loader: require.resolve("@linaria/webpack-loader"),
-//           options: {
-//             sourceMap: process.env.NODE_ENV !== "production",
-//             ...(config.linaria || {}),
-//             extension: LINARIA_EXTENSION,
-//           },
-//         },
-//       ],
-//     });
-//     config.module.rules.push({
-//       test: /_app\.(tsx|ts|js|mjs|jsx)$/,
-//       exclude: /node_modules/,
-//       use: [
-//         {
-//           loader: require.resolve("@linaria/webpack-loader"),
-//           options: {
-//             sourceMap: process.env.NODE_ENV !== "production",
-//             ...(config.linaria || {}),
-//             extension: ".css",
-//           },
-//         },
-//       ],
-//     });
-
-//     if (typeof config.webpack === "function") {
-//       return config.webpack(config, options);
-//     }
-//     return config;
-//   },
-// });
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+      return config;
+    },
+  };
+};
