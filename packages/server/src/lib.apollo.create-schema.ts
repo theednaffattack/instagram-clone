ad0d3d17185a1407b9e5d2328d8fedc8d586600c
Eddie Naff <eddie@eddienaff.dev>
2021-06-20 16:44:53 -0700

    Extract authorization checker function

--- packages/server/src/lib.apollo.create-schema.ts
+++ packages/server/src/lib.apollo.create-schema.ts
@@ -1,28 +1,27 @@
 import { GraphQLSchema } from "graphql";
-import { buildSchemaSync } from "type-graphql";
+import { buildSchemaSync, ResolverData } from "type-graphql";
+
 import { pubsub } from "./config.redis";
 import { ResolveTime } from "./middleware.resolve-time";
+import { GetOnlyThreads } from "./resolver.get-only-threads";
 import { HelloWorldResolver } from "./resolver.hello-world";
 import { LoginResolver } from "./resolver.login";
 import { MeResolver } from "./resolver.me";
 import { RegisterResolver } from "./resolver.register";
+import { MyContext } from "./typings";
 
 export function createSchema(): GraphQLSchema {
   return buildSchemaSync({
-    authChecker: ({ context: { req } }) => {
-      // I can read context here
-      // check permission vs what's in the db "roles" argument
-      // that comes from `@Authorized`, eg,. ["ADMIN", "MODERATOR"]
-      // return !!req.session.userId;
-      console.log(Object.keys(req));
-
-      return true;
-    },
+    authChecker: AuthorizationChecker,
     dateScalarMode: "isoDate",
     pubSub: pubsub,
     // Keep 'resolvers' below alphabetical please!
-    resolvers: [HelloWorldResolver, LoginResolver, MeResolver, RegisterResolver],
-
+    resolvers: [GetOnlyThreads, HelloWorldResolver, LoginResolver, MeResolver, RegisterResolver],
     globalMiddlewares: [ResolveTime],
   });
+}
+
+function AuthorizationChecker({ context: { req } }: ResolverData<MyContext>): boolean {
+  console.log(Object.keys(req));
+  return true;
 }
