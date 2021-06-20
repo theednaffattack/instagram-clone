f3951bd9651eda5d04cad777e5f08ac351449040
Eddie Naff <eddie@eddienaff.dev>
2021-06-19 17:11:58 -0700

    Add entities needed for posts

---
+++ packages/server/src/entity.like.ts
@@ -0,0 +1,32 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, ManyToOne, Column } from "typeorm";
+import { Field, ID, ObjectType, Int } from "type-graphql";
+
+import { Post } from "./entity.post";
+import { User } from "./entity.user";
+
+@ObjectType()
+@Entity()
+export class Like extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Field(() => ID)
+  @Column()
+  postId: string;
+
+  @Field(() => Post)
+  @ManyToOne(() => Post, (post) => post.likes)
+  post: Post;
+
+  @Field(() => ID)
+  @Column()
+  userId: string;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.likes)
+  user: User;
+
+  @Field(() => Int, { nullable: false })
+  count: number;
+}
