38fde91f3b246b83cbecb5b2851ac21f89ea9752
Eddie Naff <eddie@eddienaff.dev>
2021-06-20 16:37:00 -0700

    Add me resolver class

---
+++ packages/server/src/resolver.me.ts
@@ -0,0 +1,24 @@
+import { Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
+import { User } from "./entity.user";
+import { isAuth } from "./middleware.is-auth";
+import { MyContext } from "./typings";
+
+@Resolver()
+export class MeResolver {
+  @UseMiddleware(isAuth)
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
+    // The below if statement SHOULD be impossible due to the 'isAuth'
+    // middleware.
+    if (!ctx.userId) {
+      return undefined;
+    }
+
+    try {
+      return User.findOne(ctx.userId);
+    } catch (error) {
+      console.error(error);
+      throw Error(error);
+    }
+  }
+}
