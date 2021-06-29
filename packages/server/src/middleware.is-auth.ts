c0d5042514998523d2de6f008708f586b0653b0b
Eddie Naff <eddie@eddienaff.dev>
2021-06-29 10:07:15 -0700

    Add logs for debugging

--- packages/server/src/middleware.is-auth.ts
+++ packages/server/src/middleware.is-auth.ts
@@ -4,6 +4,11 @@ import { MyContext } from "./typings";
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   // if (!context.req.session!.userId && !context.userId) {
   if (!context.userId) {
+    console.log("VIEW CONTEXT OBJECT KEYS");
+    console.log(Object.keys(context));
+    console.log(context.req.session);
+    console.log("VIEW USER ID", { userId: context.userId });
+
     throw new Error("Not authenticated");
   }
   return next();
