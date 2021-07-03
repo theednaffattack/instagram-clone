091f38d2392e4d0d8178f2e3c9d2feba0c740797
Eddie Naff <eddie@eddienaff.dev>
2021-07-02 17:20:13 -0700

    Change resolver to return null

--- packages/server/src/resolver.me.ts
+++ packages/server/src/resolver.me.ts
@@ -7,18 +7,25 @@ import { MyContext } from "./typings";
 export class MeResolver {
   @UseMiddleware(isAuth)
   @Query(() => User, { nullable: true })
-  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
+  async me(@Ctx() ctx: MyContext): Promise<User | null> {
     // The below if statement SHOULD be impossible due to the 'isAuth'
     // middleware.
     if (!ctx.userId) {
-      return undefined;
+      return null;
     }
 
+    let user;
     try {
-      return ctx.dbConnection.getRepository(User).findOne(ctx.userId);
+      user = await ctx.dbConnection.getRepository(User).findOne(ctx.userId);
     } catch (error) {
       console.error(error);
       throw Error(error);
+    }
+
+    if (user) {
+      return user;
+    } else {
+      return null;
     }
   }
 }
