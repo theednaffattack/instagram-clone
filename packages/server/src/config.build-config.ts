08fc3b20a171f4297c95b7a627f6bcd1d4736ccd
Eddie Naff <eddie@eddienaff.dev>
2021-07-09 20:15:53 -0700

    Adjust config to include AWS cdn link and cdn domain

--- packages/server/src/config.build-config.ts
+++ packages/server/src/config.build-config.ts
@@ -12,10 +12,12 @@ export interface ServerConfigProps {
   awsConfig: {
     awsAccessKeyId: string;
     awsSecretAccessKey: string;
-    cfDomain: string;
+    cfCdnDomain: string;
+    cfDomainForUri: string;
     cfPublicKeyId: string;
     cfPublicKey: string;
     cfPrivateKey: string;
+    cfDomain: string;
     s3Bucket: string;
   };
   client_uri: string;
@@ -44,202 +46,216 @@ export interface ServerConfigProps {
   secret: string;
 }
 
-// Define a schema
-export const configBuildAndValidate = async function () {
-  const configBuilder = convict({
-    allowedOrigins: {
-      doc: "Allowed list of CORS origins",
-      default: "ic.eddienaff.dev",
-      env: "ALLOWED_ORIGINS",
-      format: String,
+const configBuilder = convict({
+  allowedOrigins: {
+    doc: "Allowed list of CORS origins",
+    default: "ic.eddienaff.dev",
+    env: "ALLOWED_ORIGINS",
+    format: String,
+  },
+  apiEndpoint: {
+    default: "/api",
+    doc: "The slash address of our GraphQl uri",
+    env: "API_ENDPOINT",
+    format: String,
+  },
+  awsConfig: {
+    awsAccessKeyId: {
+      default: "**********",
+      doc: "AWS Access Key ID used as sort of an identifier / user ID.",
+      env: "AWS_ACCESS_KEY_ID",
+      format: "*",
+      sensitive: true,
     },
-    apiEndpoint: {
-      default: "/api",
-      doc: "The slash address of our GraphQl uri",
-      env: "API_ENDPOINT",
-      format: String,
+    awsSecretAccessKey: {
+      default: "**********",
+      doc: "AWS Secret key used to access various services. This project uses S3 and CloudFront.",
+      env: "AWS_SECRET_KEY",
+      format: "*",
+      sensitive: true,
     },
-    awsConfig: {
-      awsAccessKeyId: {
-        default: "**********",
-        doc: "AWS Access Key ID used as sort of an identifier / user ID.",
-        env: "AWS_ACCESS_KEY_ID",
-        format: "*",
-        sensitive: true,
-      },
-      awsSecretAccessKey: {
-        default: "**********",
-        doc: "AWS Secret key used to access various services. This project uses S3 and CloudFront.",
-        env: "AWS_SECRET_KEY",
-        format: "*",
-        sensitive: true,
-      },
-      cfDomain: {
-        default: "**********",
-        doc: "Cloudfront domain used for file URIs",
-        env: "CF_DOMAIN",
-        format: "*",
-        sensitive: true,
-      },
-      cfPublicKey: {
-        default: "**********",
-        doc: "Cloudfront Public Key for key group name 'ic-server-too",
-        env: "CF_PUBLIC_KEY",
-        format: "*",
-        sensitive: true,
-      },
-      cfPublicKeyId: {
-        default: "**********",
-        doc: "Cloudfront Private Key for key group name 'ic-server-too'",
-        env: "CF_KEY_GROUP_PUBLIC_KEY_ID",
-        format: "*",
-        sensitive: true,
-      },
-      cfPrivateKey: {
-        default: "**********",
-        doc: "Cloudfront Private Key for key group name 'ic-server-too'",
-        env: "CF_PRIVATE_KEY",
-        format: "*",
-        sensitive: true,
-      },
-      s3Bucket: {
-        default: "**********",
-        doc: "Info for connecting to S3",
-        env: "S3_BUCKET",
-        format: "*",
-        sensitive: true,
-      },
+    cfCdnDomain: {
+      default: "iccdn.eddienaff.dev",
+      doc: "Custom domain used for Cloudfront file URIs residing in S3 Buckets.",
+      env: "CF_CDN_DOMAIN",
+      format: "*",
+      sensitive: true,
     },
-    client_uri: {
-      default: "https://ic.eddienaff.dev",
-      doc: "The URI of our client application.",
-      format: "url",
+    cfDomain: {
+      default: "**********",
+      doc: "Cloudfront domain used for cookie settings.",
+      env: "CF_DOMAIN",
+      format: "*",
+      sensitive: true,
     },
-    cookieDomain: {
-      default: "eddienaff.dev",
-      doc: "The domain setting in our server cookie.",
-      env: "COOKIE_DOMAIN",
-      format: String,
+    cfDomainForUri: {
+      default: "**********",
+      doc: "Cloudfront domain used for file URIs. (Like in AWS Resource(s))",
+      env: "CF_DOMAIN_FOR_URI",
+      format: "*",
+      sensitive: true,
     },
-    cookieName: {
-      default: "icg",
-      doc: "Cookie name used for our sessions.",
-      env: "COOKIE_NAME",
-      format: String,
+    cfPublicKey: {
+      default: "**********",
+      doc: "Cloudfront Public Key for key group name 'ic-server-too",
+      env: "CF_PUBLIC_KEY",
+      format: "*",
+      sensitive: true,
     },
-    domain: {
-      default: "ic.eddienaff.dev",
-      doc: "The hostname to use for linking and documentation references.",
-      env: "DOMAINS",
+    cfPublicKeyId: {
+      default: "**********",
+      doc: "Cloudfront Private Key for key group name 'ic-server-too'",
+      env: "CF_PUBLIC_KEY_ID",
+      format: "*",
+      sensitive: true,
+    },
+    cfPrivateKey: {
+      default: "**********",
+      doc: "Cloudfront Private Key for key group name 'ic-server-too'",
+      env: "CF_PRIVATE_KEY",
+      format: "*",
+      sensitive: true,
+    },
+    s3Bucket: {
+      default: "**********",
+      doc: "Info for connecting to S3",
+      env: "S3_BUCKET",
+      format: "*",
+      sensitive: true,
+    },
+  },
+  client_uri: {
+    default: "https://ic.eddienaff.dev",
+    doc: "The URI of our client application.",
+    format: "url",
+  },
+  cookieDomain: {
+    default: ".eddienaff.dev",
+    doc: "The domain setting in our server cookie.",
+    env: "COOKIE_DOMAIN",
+    format: String,
+  },
+  cookieName: {
+    default: "icg",
+    doc: "Cookie name used for our sessions.",
+    env: "COOKIE_NAME",
+    format: String,
+  },
+  domain: {
+    default: "ic.eddienaff.dev",
+    doc: "The hostname to use for linking and documentation references.",
+    env: "DOMAINS",
+    format: String,
+  },
+  host: {
+    default: "0.0.0.0",
+    doc: "Hostname we bind the server to",
+    env: "DOMAINS",
+    format: String,
+  },
+  db: {
+    connectionString: {
+      default: "postgres://spc_system:R4TDP6g37Lzkh3Fc5U8zGvfZx7@localhost:5432/spotify_clone",
+      doc: "The database connection string",
+      env: "DATABASE_URL",
       format: String,
     },
     host: {
-      default: "0.0.0.0",
-      doc: "Hostname we bind the server to",
-      env: "DOMAINS",
+      default: "ic.eddienaff.dev",
+      doc: "Database host name/IP",
+      format: "*",
+    },
+    name: {
+      default: "spotify_clone",
+      doc: "Database name",
+      env: "POSTGRES_DBNAME",
       format: String,
     },
-    db: {
-      connectionString: {
-        default: "postgres://spc_system:R4TDP6g37Lzkh3Fc5U8zGvfZx7@localhost:5432/spotify_clone",
-        doc: "The database connection string",
-        env: "DATABASE_URL",
-        format: String,
-      },
-      host: {
-        default: "ic.eddienaff.dev",
-        doc: "Database host name/IP",
-        format: "*",
-      },
-      name: {
-        default: "spotify_clone",
-        doc: "Database name",
-        env: "POSTGRES_DBNAME",
-        format: String,
-      },
-      username: {
-        default: "ic_user",
-        doc: "Postgres username for database login.",
-        env: "POSTGRES_USER",
-        format: String,
-      },
-      password: {
-        default: "*******",
-        doc: "Postgres password for database login.",
-        env: "POSTGRES_PASSWORD",
-        format: String,
-      },
-      port: {
-        default: "5432",
-        doc: "Postgres connection port",
-        env: "POSTGRES_PORT",
-        format: Number,
-      },
+    username: {
+      default: "ic_user",
+      doc: "Postgres username for database login.",
+      env: "POSTGRES_USER",
+      format: String,
     },
-    env: {
-      default: "development",
-      doc: "The application environment.",
-      env: "NODE_ENV",
-      format: ["production", "local_production", "development", "test"],
-    },
-    ip: {
-      default: "127.0.0.1",
-      doc: "The IP address to bind our server to.",
-      env: "IP_ADDRESS",
-      format: "ipaddress",
+    password: {
+      default: "*******",
+      doc: "Postgres password for database login.",
+      env: "POSTGRES_PASSWORD",
+      format: String,
     },
     port: {
-      arg: "port",
-      default: 8080,
-      doc: "The port to bind.",
-      env: "PORT",
-      format: "port",
+      default: "5432",
+      doc: "Postgres connection port",
+      env: "POSTGRES_PORT",
+      format: Number,
     },
-    postmarkToken: {
-      default: "",
-      doc: "Secret token used to access the Postmark mailing API.",
-      env: "POSTMARK_API_TOKEN",
-      format: "*",
-      sensitive: true,
+  },
+  env: {
+    default: "development",
+    doc: "The application environment.",
+    env: "NODE_ENV",
+    format: ["production", "local_production", "development", "test"],
+  },
+  ip: {
+    default: "127.0.0.1",
+    doc: "The IP address to bind our server to.",
+    env: "IP_ADDRESS",
+    format: "ipaddress",
+  },
+  port: {
+    arg: "port",
+    default: 8080,
+    doc: "The port to bind.",
+    env: "PORT",
+    format: "port",
+  },
+  postmarkToken: {
+    default: "",
+    doc: "Secret token used to access the Postmark mailing API.",
+    env: "POSTMARK_API_TOKEN",
+    format: "*",
+    sensitive: true,
+  },
+  redis: {
+    connectionString: {
+      default: "redis://:<REDIS_PASSWORD???>@dokku-redis-<DOKKU_REDIS_SERVICE_NAME>:6379",
+      doc: "The redis instance connection string",
+      env: "REDIS_URL",
+      format: String,
     },
-    redis: {
-      connectionString: {
-        default: "redis://:<REDIS_PASSWORD???>@dokku-redis-<DOKKU_REDIS_SERVICE_NAME>:6379",
-        doc: "The redis instance connection string",
-        env: "REDIS_URL",
-        format: String,
-      },
-      host: {
-        default: "dokku-redis-ic-redis",
-        doc: "The name of the redis host used to create the connection.",
-        env: "REDIS_HOST",
-        format: String,
-      },
-      interiorPort: {
-        default: "6379",
-        doc: "The number of the redis port used to create the connection.",
-        env: "REDIS_INTERIOR_PORT",
-        format: Number,
-      },
-      exteriorPort: {
-        default: "56379",
-        doc: "The number of the redis port used to create the connection.",
-        env: "REDIS_EXTERIOR_PORT",
-        format: Number,
-      },
+    host: {
+      default: "dokku-redis-ic-redis",
+      doc: "The name of the redis host used to create the connection.",
+      env: "REDIS_HOST",
+      format: String,
     },
-    secret: {
-      default: "",
-      doc: "Secret used for session cookies and CSRF tokens",
-      env: "SESSION_SECRET",
-      format: "*",
-      sensitive: true,
+    interiorPort: {
+      default: "6379",
+      doc: "The number of the redis port used to create the connection.",
+      env: "REDIS_INTERIOR_PORT",
+      format: Number,
     },
-  });
+    exteriorPort: {
+      default: "56379",
+      doc: "The number of the redis port used to create the connection.",
+      env: "REDIS_EXTERIOR_PORT",
+      format: Number,
+    },
+  },
+  secret: {
+    default: "",
+    doc: "Secret used for session cookies and CSRF tokens",
+    env: "SESSION_SECRET",
+    format: "*",
+    sensitive: true,
+  },
+});
 
-  // Extract properties to a POJO
-  const config = configBuilder.getProperties();
+// Extract properties to a POJO
+const config = configBuilder.getProperties();
 
+// Define a schema
+export const configBuildAndValidate = async function () {
   let host: string | undefined;
   let finalConfig;
   if (config.env === "development") {
