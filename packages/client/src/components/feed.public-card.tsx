8e89a4ca017c4dd479bb9f2573770d6d31f2d0fd
Eddie Naff <eddie@eddienaff.dev>
2021-07-10 16:24:06 -0700

    Add menu to feed card 'more' button

--- packages/client/src/components/feed.public-card.tsx
+++ packages/client/src/components/feed.public-card.tsx
@@ -3,6 +3,10 @@ import {
   Box,
   Flex,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Skeleton,
   Text,
 } from "@chakra-ui/react";
@@ -95,18 +99,43 @@ export function PublicPostCard({ cardProps }: CardProps): JSX.Element {
         src={images && images[0] ? images[0].uri : ""}
       /> */}
       <Flex alignItems="center" pl={4} pr={2} py={3}>
-        <Avatar name="Oshigaki Kisame" />
+        <Avatar name="Hoshigaki Kisame" tabIndex={0} />
         <Flex flexDirection="column" pl={2}>
-          <p>Oshigaki Kisame</p>
-          <p>LOCATION TEXT</p>
+          <Text tabIndex={0}>Oshigaki Kisame</Text>
+          <Text tabIndex={0}>LOCATION TEXT</Text>
         </Flex>
-
-        <IconButton
-          aria-label="more"
-          icon={<BiDotsHorizontalRounded fill="grey" size={28} />}
-          ml="auto"
-          colorScheme="whiteAlpha"
-        ></IconButton>
+        {/**
+         * TODO: MAKE THIS MENU A MODAL OR... PORTAL, I GUESS??
+         */}
+        <Menu isLazy>
+          <MenuButton
+            aria-label="more"
+            as={IconButton}
+            bg="transparent"
+            ml="auto"
+            icon={<BiDotsHorizontalRounded fill="grey" size={28} />}
+          >
+            Open menu
+          </MenuButton>
+          <MenuList>
+            <MenuItem>
+              <Text color="crimson">Report</Text>
+            </MenuItem>
+            <MenuItem onClick={() => alert("Copy Link pressed")}>
+              Copy Link
+            </MenuItem>
+            <MenuItem onClick={() => alert("Share to... pressed")}>
+              Share to...
+            </MenuItem>
+            <MenuItem onClick={() => alert("Notifications pressed")}>
+              Turn On Post Notifications
+            </MenuItem>
+            <MenuItem onClick={() => alert("Mute pressed")}>Mute</MenuItem>
+            <MenuItem onClick={() => alert("Unfollow pressed")}>
+              Unfollow
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
       <Box>
         {images && images[0] ? (
