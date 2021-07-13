7667b3cec085cdb299c504d92f7d38c7a8b07ba5
Eddie Naff <eddie@eddienaff.dev>
2021-07-12 18:15:00 -0700

    Prepare to ditch logout route

--- packages/client/src/pages/logout.tsx
+++ packages/client/src/pages/logout.tsx
@@ -1,66 +1,73 @@
-import { LogoutDocument, LogoutMutation } from "../generated/graphql";
-import { initializeApollo } from "../lib/lib.apollo-client";
-import { MyContext } from "../lib/types";
-import { redirect } from "../lib/utilities.redirect";
+import router from "next/router";
+import React, { useEffect } from "react";
+import { ErrorMessage } from "../components/error-message";
+import { useLogoutMutation } from "../generated/graphql";
 
-type LogoutServerSideProps = Promise<{
-  props: {
-    initialApolloState: Record<string, unknown>;
-    revalidate: number;
-    response: any;
-  };
-}>;
+// type LogoutServerSideProps = Promise<{
+//   props: {
+//     initialApolloState: Record<string, unknown>;
+//     revalidate: number;
+//     response: any;
+//   };
+// }>;
 
 const Logout = (): JSX.Element | void => {
-  return null;
-  // const [logoutFunc, { data, error, loading }] = useLogoutMutation();
-  // useEffect(() => {
-  //   logoutFunc();
-  // }, []);
-  // if (error) {
-  //   return <ErrorMessage message={error.message} />;
-  // }
-  // if (loading) {
-  //   return <div>loading...</div>;
-  // }
-  // if (data) {
-  //   if (router) {
-  //     router.push("/login");
-  //   }
-  // }
+  const [logoutFunc, { data, error, loading }] = useLogoutMutation();
+  useEffect(() => {
+    logoutFunc();
+  }, []);
+  // eslint-disable-next-line no-console
+  console.log("VIEW APOLLO LOGOUT VALUES", { data, error, loading });
+
+  if (error) {
+    return <ErrorMessage message={error.message} />;
+  }
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  if (data) {
+    if (router) {
+      router.push("/login");
+    }
+  }
+  return <div>LOUGOUT</div>;
 };
 
-export async function getServerSideProps(
-  ctx: MyContext
-): Promise<LogoutServerSideProps> {
-  if (!ctx.apolloClient) ctx.apolloClient = initializeApollo();
+// export async function getServerSideProps(ctx: MyContext): Promise<any> {
+//   if (!ctx.apolloClient) ctx.apolloClient = initializeApollo();
 
-  try {
-    // await ctx.apolloClient.resetStore();
-    await ctx.apolloClient.cache.reset();
-  } catch (error) {
-    console.error("APOLLO RESET STORE", error);
-    throw Error("Error resetting Apollo cache.");
-  }
-  let response;
-  try {
-    response = await ctx.apolloClient.mutate<LogoutMutation>({
-      mutation: LogoutDocument,
-    });
-  } catch (error) {
-    console.error("APOLLO MUTATE ERROR", error);
-    throw Error("Error logging out.");
-  }
+//   try {
+//     // await ctx.apolloClient.resetStore();
+//     await ctx.apolloClient.cache.reset();
+//   } catch (error) {
+//     console.error("APOLLO RESET STORE", error);
+//     throw Error("Error resetting Apollo cache.");
+//   }
 
-  redirect(ctx, "/login");
+//   try {
+//     await ctx.apolloClient.mutate<LogoutMutation>({
+//       mutation: LogoutDocument,
+//     });
 
-  return {
-    props: {
-      initialApolloState: {}, //apolloClient.cache.extract()
-      revalidate: 1,
-      response,
-    },
-  };
-}
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   } catch (error) {
+//     console.error("APOLLO MUTATE ERROR", error);
+//     throw Error("Error logging out.");
+//   }
+
+//   // return { props: {} };
+//   // return {
+//   //   props: {
+//   //     initialApolloState: {}, //apolloClient.cache.extract()
+//   //     revalidate: 1,
+//   //     response,
+//   //   },
+//   // };
+// }
 
 export default Logout;
