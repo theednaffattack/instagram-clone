c0687acefcffa624e9ccdaa0e83f89e37ae8c5e8
Eddie Naff <eddie@eddienaff.dev>
2021-07-18 17:10:53 -0700

    Switch auth middleware back to JWTs from cookies

--- packages/server/src/middleware.is-auth.ts
+++ packages/server/src/middleware.is-auth.ts
@@ -1,10 +1,35 @@
 import { MiddlewareFn } from "type-graphql";
+import { verify } from "jsonwebtoken";
+import { configBuildAndValidate } from "./config.build-config";
 import { MyContext } from "./typings";
+import { logger } from "./lib.logger";
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  // if (!context.req.session!.userId && !context.userId) {
-  if (!context.userId) {
+  const authorization = context.req.headers["authorization"];
+  const config = await configBuildAndValidate();
+  if (!authorization) {
     throw new Error("Not authenticated");
   }
+
+  let token;
+  try {
+    token = authorization.split(" ")[1];
+  } catch (error) {
+    console.error(error);
+    throw new Error("Not authenticated");
+  }
+
+  let payload;
+
+  try {
+    payload = verify(token, config.accessTokenSecret);
+    if (typeof payload !== "string") {
+      context.payload = payload;
+    }
+  } catch (error) {
+    console.error("ERROR VERIFYING JWT");
+    console.error(error);
+  }
+
   return next();
 };
