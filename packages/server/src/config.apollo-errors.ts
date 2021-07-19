f795f4a566aa92217a4d29c8e72f4c8d251ec5b2
Eddie Naff <eddie@eddienaff.dev>
2021-07-18 17:07:08 -0700

    Tryout custom errors

--- packages/server/src/config.apollo-errors.ts
+++ packages/server/src/config.apollo-errors.ts
@@ -1,19 +1,45 @@
-import { createError } from "apollo-errors";
+import { ApolloError, createError, ErrorConfig } from "apollo-errors";
 
 enum CustomErrorType {
   AuthorizationError = "AuthorizationError",
+  DatabaseError = "DatabaseError",
 }
 
 export const nodeEnvIsUndefined = (functionName: string): string =>
   `The NODE_ENV var is undefined. The "${functionName}" expects to receive NODE_ENV as an argument.  Please set your environment variable "NODE_ENV" to "development", "production" or "test" and try again.`;
 
-export const errorSavingInfoToDatabase = (functionName?: string, error?: unknown | string): string =>
-  `${functionName} experienced an error while saving your information to the database.\n${error}`;
+export function errorSavingInfoToDatabase({ functionName, error }: { functionName: string; error: any }) {
+  return createError(CustomErrorType.DatabaseError, {
+    message: "",
+    options: { showLocations: true, showPath: true },
+  });
+}
 
-export const AuthorizationError = createError("AuthorizationError", {
-  message: "You are not authorized.",
-});
+// export const AuthorizationError = createError("AuthorizationError", {
+//   message: "You are not authorized.",
+// });
 
-export function createAuthorizationError({ message = "You are not authorized" }) {
+interface CreateAuthorizationErrorProps {
+  message?: string | undefined;
+}
+export function createAuthorizationError({ message = "You are not authorized" }: CreateAuthorizationErrorProps) {
   return createError(CustomErrorType.AuthorizationError, { message });
 }
+
+export function createAuthenticationError({ message = "Not Authenticated" }: CreateAuthorizationErrorProps) {
+  return createError(CustomErrorType.AuthorizationError, { message });
+}
+
+// export class MyError extends ApolloError {
+//   constructor(message: string) {
+//     super(message, "MY_ERROR_CODE", {});
+
+//     Object.defineProperty(this, "name", { value: "MyError" });
+//   }
+// }
+
+export const JwtExpirationError = new ApolloError(
+  "Your session has expired, please log in.",
+  { message: "fake messsage one" },
+  { message: "fake messsage two" }
+);
