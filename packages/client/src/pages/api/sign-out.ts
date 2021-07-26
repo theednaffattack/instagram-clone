b906393547d4eb28673f751e1aa7493bb3b85204
Eddie Naff <eddie@eddienaff.dev>
2021-07-25 19:55:24 -0700

    Git rid of next-auth code

--- packages/client/src/pages/api/sign-out.ts
+++ packages/client/src/pages/api/sign-out.ts
@@ -1,7 +1,6 @@
 // api/sign-out
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { getCsrfToken } from "next-auth/client";
 import { logger } from "../../lib/lib.logger";
 
 export default async function signOutHandler(
@@ -14,19 +13,9 @@ export default async function signOutHandler(
     `CloudFront-Key-Pair-Id=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
     `CloudFront-Policy=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
     `icg=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
-    `next-auth.csrf-token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
   ]);
-  // eslint-disable-next-line no-console
-  logger.info("REMOVING CUSTOM COOKIES AND CLOUDFRONT COOKIES");
 
-  let csrfToken;
-  try {
-    csrfToken = await getCsrfToken({ req });
-  } catch (error) {
-    logger.error(error, "ERROR GETTING TOKEN");
-  }
-  logger.info("TOKEN?");
-  logger.info(csrfToken);
+  logger.info("REMOVING CUSTOM COOKIES AND CLOUDFRONT COOKIES");
 
   res.status(200);
   return res.send(true);
