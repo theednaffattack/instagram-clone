0f1e93c564dc63143866b6b1efd1f0eff86f0660
Eddie Naff <eddie@eddienaff.dev>
2021-07-27 12:55:10 -0700

    Read userId from context payload

--- packages/server/src/resolver.me.ts
+++ packages/server/src/resolver.me.ts
@@ -1,5 +1,6 @@
 import { Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "./entity.user";
+import { logger } from "./lib.logger";
 import { isAuth } from "./middleware.is-auth";
 import { MyContext } from "./typings";
 
@@ -7,8 +8,9 @@ import { MyContext } from "./typings";
 export class MeResolver {
   @UseMiddleware(isAuth)
   @Query(() => String)
-  async bye(@Ctx() { payload }: MyContext): Promise<string> {
-    return `bye ${payload.user.id}`;
+  async bye(@Ctx() ctx: MyContext): Promise<string> {
+    logger.info(ctx.payload, "CHECK PAYLOAD");
+    return `bye ${ctx.payload.userId}`;
   }
 
   @UseMiddleware(isAuth)
@@ -16,7 +18,7 @@ export class MeResolver {
   async me(@Ctx() ctx: MyContext): Promise<User | null> {
     // The below if statement SHOULD be impossible due to the 'isAuth'
     // middleware.
-    if (!ctx.userId) {
+    if (!ctx.payload.userId) {
       return null;
     }
 
