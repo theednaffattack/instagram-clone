a9ba05c51ce2f7b5dea2f79e265a0ebca40c9cae
Eddie Naff <eddie@eddienaff.dev>
2021-08-01 19:24:21 -0700

    Add meQuery to component to prevent FOUC on auth

---
+++ packages/client/src/components/protected.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useMeQuery } from "../generated/graphql";
+import { logger } from "../lib/lib.logger";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export function Protected({ children }: ProtectedProps): JSX.Element {
+  const { data, error, loading } = useMeQuery();
+  if (error) {
+    logger.error(error);
+    return null;
+  }
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  if (data && data.me) {
+    // Otherwise render the create post page
+    return <>{children}</>;
+  }
+  // We hope this is impossible?
+}
