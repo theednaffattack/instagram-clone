40a4ffb563b3efe524fa6e42c255f6778a6c5b69
Eddie Naff <eddie@eddienaff.dev>
2021-08-10 16:15:24 -0700

    Add note to config

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -4,4 +4,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+// adapted from: https://frontend-digest.com/how-to-combine-and-use-multiple-nextjs-plugins-7425ab03c007
+
 module.exports = withPlugins([withLinaria, withBundleAnalyzer]);
